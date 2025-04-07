db6cb032c85a0ae5213e0c6620f2778fb3b38914
Pasha Rezero <kivachuk.2018@gmail.com>
2025-04-07 14:14:26 +0300

    hot fix style

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Navbar } from "./component/navigation/Navbar";
 
 export const App = () => {
   return (
-    <div className="bg-[linear-gradient(180deg,_#EBF2FC_0%,_#EEF8F9_100%)] dark:bg-[linear-gradient(180deg,_#040918_0%,_#091540_100%)] pt-8 w-[100%] h-[100%] md:h-screen">
+    <div className="bg-[linear-gradient(180deg,_#EBF2FC_0%,_#EEF8F9_100%)] dark:bg-[linear-gradient(180deg,_#040918_0%,_#091540_100%)] py-8 w-[100%] h-[100%]">
       <div className="container mx-auto w-[90%] md:w-[85%]">
         <Navbar />
         <ExtentionsList />
